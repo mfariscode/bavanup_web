9c4fde7343411ae0bd82299ac2185c06246b01d2
Mohammed Faris M A <mfaris2k18@gmail.com>
2025-04-15 10:02:31 +0530

    Update 3.0.0 3

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "4f89ff2dfc45fd71e0dec3cd046ccd90",
+const RESOURCES = {"flutter_bootstrap.js": "739ae8382e54bf70d3575208e5472a38",
 "version.json": "df36989b4e68288bf82a074b293fbb44",
 "index.html": "de86f144b26fe6f9dee6314e5fcde0bd",
 "/": "de86f144b26fe6f9dee6314e5fcde0bd",
-"main.dart.js": "129577133aeed16f8fc881da89489e5e",
+"main.dart.js": "81778daa007699b1ad61f7bd0f017628",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
