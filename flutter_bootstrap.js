d08bb6feb119a4fe97db01b6afb4d52beaf968b4
Mohammed Faris M A <mfaris2k18@gmail.com>
2026-05-08 16:05:52 +0530

    Refactor code structure for improved readability and maintainability

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "213888028"
+    serviceWorkerVersion: "2270289587"
   }
 });
